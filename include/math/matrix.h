5624a9f6065434a3e88f41c764a4121d7937d3ab
okayscylla <75046957+okayscylla@users.noreply.github.com>
2025-02-11 20:09:53 -0500

    start matrix changes

--- include/math/matrix.h
+++ include/math/matrix.h
@@ -1,1 +1,12 @@
-// Someday...
+#pragma once
+#include "math/vec3.h"
+#include <array>
+
+class Mat4 {
+    public:
+    Mat4 operator*(const Mat4& term) const;
+    Mat4 operator*(const Vec3& term) const;
+
+    private:
+    std::array<double, 16> data;
+}
